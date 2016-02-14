74b7867f767359d8b2b75a951cf22723c64bae45
valverde <adam.pociejowski@gmail.com>
2016-02-14 16:07:02 +0100

    Added sorted alfabetically playlist.

--- app/src/main/java/com/example/adam/mp3player/files_scanner/FilesScannerFragment.java
+++ app/src/main/java/com/example/adam/mp3player/files_scanner/FilesScannerFragment.java
@@ -14,32 +14,53 @@ import java.util.ArrayList;
 
 public class FilesScannerFragment extends Fragment {
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.main_files_scanner_fragment, container, false);
     }
+
 
     @Override
     public void onStart() {
         super.onStart();
         createList();
     }
-    
+
 
     public void createList() {
         ArrayList<Song> songsList = Config.getInstance().getSongsList();
         if (songsList == null) {
+            Log.d("song", "song");
             songsList = new ArrayList<>();
             File directory = new File(Config.getInstance().getMusicInternalPath());
             try {
                 File files[] = directory.listFiles();
-                for (File file : files) songsList.add(new Song(file.getName().substring(0, file.getName().length() - 4), file.getAbsolutePath()));
-                    Config.getInstance().setSongsList(songsList);
+                for (File file : files) {
+                    songsList.add(new Song(file.getName().substring(0, file.getName().length() - 4), file.getAbsolutePath()));
                 }
+                songsList = getSortedSongs(songsList);
+                Config.getInstance().setSongsList(songsList);
+            }
             catch (Exception e) {
                 Log.e("error", "Error while reading files from " + Config.getInstance().getMusicInternalPath() + " - FilesScannerFragment.java");
             }
         }
         new FileScannerListViewCreator(songsList, getActivity());
+    }
+
+
+    private ArrayList<Song> getSortedSongs(ArrayList<Song> songs) {
+        Song s;
+        int i, j;
+
+        for (j = 0; j < songs.size(); j++) {
+            s = songs.get(j);
+            for (i = j - 1; (i >= 0) && (songs.get(i).getTitle().compareTo(s.getTitle()) > 0); i--) {
+                songs.set(i + 1, songs.get(i));
+            }
+            songs.set(i + 1, s);
+        }
+        return songs;
     }
 }
